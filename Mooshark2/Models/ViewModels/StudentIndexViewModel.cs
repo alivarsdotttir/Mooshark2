3ad72f6c13fc5503309e0e04dbb783477e6f3994
tinnathuridur <tinnats15@ru.is>
2016-05-04 11:57:08 +0000

    komin eigindi í Student ViewModel klasana, en ég er óviss á privacy fyrir listana

--- Mooshark2/Models/ViewModels/StudentIndexViewModel.cs
+++ Mooshark2/Models/ViewModels/StudentIndexViewModel.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Mooshark2.Models.Entities;
+
 
 namespace Mooshark2.Models.ViewModels
 {
     public class StudentIndexViewModel
     {
+        private List<Project> upcomingProjects;
+        private List<Course> coursesForStudents;
+
+        public List<Course> CoursesForStudents
+        {
+            get
+            {
+                return coursesForStudents;
+            }
+
+            set
+            {
+                coursesForStudents = value;
+            }
+        }
+
+        public List<Project> UpcomingProjects
+        {
+            get
+            {
+                return upcomingProjects;
+            }
+
+            set
+            {
+                upcomingProjects = value;
+            }
+        }
     }
 }
