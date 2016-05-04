12f8f4c18c824b4149af2aa239e1c449ca8b00de
tinnathuridur <tinnats15@ru.is>
2016-05-04 17:26:32 +0000

    byrjaðar að fikta í StudentController og erum að athuga hvort Student ViewModelin haldast inni...

--- Mooshark2/Controllers/StudentController.cs
+++ Mooshark2/Controllers/StudentController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mooshark2.Models.DAL;
+using Mooshark2.Service;
+
 
 namespace Mooshark2.Controllers
 {
     public class StudentController : Controller
     {
+        //private ApplicationDbContext db;
+        //private CourseService courseService = new CourseService();
         // GET: Student
         public ActionResult Index()
         {
-
 
             return View();
         }
