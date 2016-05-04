dc32b1add2ad0994f37f24b3956e3b9ddccea40f
tinnathuridur <tinnats15@ru.is>
2016-05-04 15:21:36 +0000

    setja student view modelin aftur inn

---
+++ Mooshark2/Models/ViewModels/StudentDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mooshark2.Models.Entities;
+
+
+namespace Mooshark2.Models.ViewModels
+{
+    public class StudentDetailsViewModel
+    {
+        Project currentProject;
+        List<Submission> projectSubmissions;
+    }
+}
