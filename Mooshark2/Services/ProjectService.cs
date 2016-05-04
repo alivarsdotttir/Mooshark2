6b91bcaaafe88cd4022890f2c0114693ef8db856
juliaodds18 <juliao15@ru.is>
2016-05-04 12:14:54 +0000

    Logic in course service

--- Mooshark2/Services/ProjectService.cs
+++ Mooshark2/Services/ProjectService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mooshark2.Models;
+using Mooshark2.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,20 @@ namespace Mooshark2.Services
 {
     public class ProjectService
     {
+        private ApplicationDbContext db;
+
+
+        public ProjectService()
+        {
+            db = new ApplicationDbContext();
+        }
+
+
+        public List<Course> getAllCourses()
+        {
+            var courses = (from x in db.Courses
+                           select x ).ToList(); 
+            return courses;
+        } 
     }
 }
