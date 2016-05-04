bf3ac66f9136be0e9d7bc1fe5e7080ae0e93435b
Unnur Sol <unnuri15@ru.is>
2016-05-04 16:28:04 +0000

    Data Initializer stuff

--- Mooshark2/Models/DAL/DataInitializer.cs
+++ Mooshark2/Models/DAL/DataInitializer.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 
 namespace Mooshark2.Models.DAL
 {
     public class DataInitializer  : System.Data.Entity.CreateDatabaseIfNotExists<ApplicationDbContext>
     {
         protected override void Seed(ApplicationDbContext context)
-        {
+        {   /*
+            Roles.CreateRole("Admin");
+            Roles.CreateRole("Teacher");
+            Roles.CreateRole("Student");
+            */
+
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            var str = RoleManager.Create(new IdentityRole("Admin"));
+
+            var admin = new ApplicationUser {Id = "010166-0169", UserName = "admin", Email = "admin@admin.is", Roles = { }, };
           
         }
     }
