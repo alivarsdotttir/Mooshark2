6ff32d70dd2315f739b1a6debf4e755d5cb047d9
Unnur Sol <unnuri15@ru.is>
2016-05-04 13:03:15 +0000

    Initializer file

---
+++ Mooshark2/Models/DAL/DataInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshark2.Models.DAL
+{
+    public class DataInitializer  : System.Data.Entity.CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+          
+        }
+    }
+}
