714790bfbea1d86e9574eed82b114fadef2af9c9
Unnur Sol <unnuri15@ru.is>
2016-05-04 14:18:26 +0000

    Message Revert "Initializer file"
    This reverts commit c3334bc1d818a0439a3d061d7bc3f50d541fd9d0, reversing
    changes made to 6ff32d70dd2315f739b1a6debf4e755d5cb047d9.

--- Mooshark2/Services/ProjectService.cs
+++ Mooshark2/Services/ProjectService.cs
@@ -1,6 +1,4 @@
-﻿using Mooshark2.Models;
-using Mooshark2.Models.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,20 +7,5 @@ namespace Mooshark2.Services
 {
     public class ProjectService
     {
-        private ApplicationDbContext db;
-
-
-        public ProjectService()
-        {
-            db = new ApplicationDbContext();
-        }
-
-
-        public List<Course> getAllCourses()
-        {
-            var courses = (from x in db.Courses
-                           select x ).ToList(); 
-            return courses;
-        } 
     }
 }
