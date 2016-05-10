064540a11a686e37aff046dbebdbdcedd4a59290
juliaodds18 <juliao15@ru.is>
2016-05-10 17:08:22 +0000

    Getting rid of error screen. We need yellow screen of death

--- Mooshark2/Handlers/CustomHandleErrorAttribute.cs
+++ Mooshark2/Handlers/CustomHandleErrorAttribute.cs
@@ -12,7 +12,7 @@ namespace Mooshark2.Handlers
     {
         public override void OnException(ExceptionContext filterContext)
         {
-
+            /*
             //Get the exception
             Exception ex = filterContext.Exception;
 
@@ -24,7 +24,7 @@ namespace Mooshark2.Handlers
             string currentController = (string)filterContext.RouteData.Values["controller"];
             string currentActionName = (string)filterContext.RouteData.Values["action"];
 
-            /*
+            
             if (ex is CustomApplicationException && currentController != "Book")
             {
                 viewName = "ErrorCustom";
@@ -37,7 +37,7 @@ namespace Mooshark2.Handlers
             {
                 viewName = "Error";
             }
-            */
+            
 
             //Create the error model information
             HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, currentController, currentActionName);
@@ -53,6 +53,7 @@ namespace Mooshark2.Handlers
 
             // Call the base class implementation:
             base.OnException(filterContext);
+            */
         }
     }
 }
