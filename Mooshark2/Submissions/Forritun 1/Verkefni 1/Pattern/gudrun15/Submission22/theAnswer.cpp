fcea25d304a9172b7e5fa8cb83a231bc540a0618
tinnathuridur <tinnats15@ru.is>
2016-05-13 17:04:41 +0000

    final changes back to the beginning tho

---
+++ Mooshark2/Submissions/Forritun 1/Verkefni 1/Pattern/gudrun15/Submission22/theAnswer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int number = 0;
+	int counter = 0;
+	while (counter != 10000){
+		cin >> number;
+		cout << number;
+		counter++;
+	}
+    
+
+    return 0;
+}
